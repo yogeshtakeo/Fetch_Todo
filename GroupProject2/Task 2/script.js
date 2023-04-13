b1d9b7fd733b49ac76ee30b1871a66d506ebdd33
Bhabishaphuyal6 <96266221+Bhabishaphuyal6@users.noreply.github.com>
2023-04-13 13:35:04 -0700

    Task 1 Done

---
+++ GroupProject2/Task 2/script.js
@@ -0,0 +1,50 @@
+
+const todoList = document.getElementById('todo-list');
+const todoForm = document.getElementById('todo-form');
+const todoTitle = document.getElementById('todo-title');
+
+// Function to fetch todos
+async function fetchTodos() {
+  const response = await fetch('hthttps://api.punkapi.com/v2/beers/random');
+  const todos = await response.json();
+  return todos;
+}
+
+// Function to display todos
+function displayTodos(todos) {
+  const todoItems = todos.map(todo => {
+    const li = document.createElement('li');
+    li.textContent = `${todo.title} - ${todo.completed ? 'Completed' : 'Not completed'}`;
+    return li;
+  });
+  
+  todoList.append(...todoItems);
+}
+
+// Function to add new todo
+function addTodo() {
+  const title = todoTitle.value.trim();
+  if (title) {
+    const todo = {
+      userId: 1,
+      title,
+      completed: false
+    };
+    
+    const li = document.createElement('li');
+    li.textContent = `${todo.title} - ${todo.completed ? 'Completed' : 'Not completed'}`;
+    todoList.append(li);
+    
+    // Clear input
+    todoTitle.value = '';
+  }
+}
+
+// Fetch and display todos on page load
+fetchTodos().then(todos => displayTodos(todos));
+
+// Add new todo on form submit
+todoForm.addEventListener('submit', event => {
+  event.preventDefault();
+  addTodo();
+});
