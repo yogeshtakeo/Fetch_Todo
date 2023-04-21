23e26665e6eb56d358efaf6fed8cdae1b2044d03
Sanjay <sanjayluitel2@gmail.com>
2023-04-21 09:18:48 -0400

    Complete Task 7

---
+++ GroupProject7/src/App.jsx
@@ -0,0 +1,11 @@
+import NavBar from "./navbar.jsx";
+import Modal from "./modal.jsx";
+import Form from "./form.jsx";
+import Card from "./card.jsx";
+import Button from "./button.jsx";
+
+function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+export default App;
