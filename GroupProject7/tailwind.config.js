1c87cdd265388244c3fccbf07bbe9b79fb68cb22
sushilkhanal <khanal.sushil47@gmail.com>
2023-04-21 16:18:06 -0500

    project 7 mostly completed only CSS left

--- GroupProject7/tailwind.config.js
+++ GroupProject7/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{jsx,js}"],
+  content: [
+    "./src/**/*.{tsx,ts,jsx,js}",
+    "./node_modules/flowbite-react/**/*.{tsx,ts,jsx,js}",
+  ],
   theme: {
     extend: {},
   },
