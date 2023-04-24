6844350def924efc1d22abcc677348a177979beb
Sanjay <sanjayluitel2@gmail.com>
2023-04-24 16:25:57 -0400

    Initial commit

---
+++ GroupProject9/src/App.tsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import Modal from "./modal.tsx";
+import Card from "./card.tsx";
+import Footer from "./footer.tsx";
+import Navbar from "./navbar.tsx";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <br></br>
+      <br></br>
+      {/* <Modal /> */}
+      <Card />
+      <Footer />
+    </>
+  );
+}
+export default App;
---
+++ GroupProject9/src/feed.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+function Feed() {
+  const [userData, setUserData] = useState([]);
+
+  return <></>;
+}
+
+export default Feed;
