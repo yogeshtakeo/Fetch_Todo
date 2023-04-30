cf3a27a9a9929fcdc84c7064e945d373ccdb9f90
sushilkhanal <khanal.sushil47@gmail.com>
2023-04-30 14:26:03 -0500

    added the counter

--- GroupProject4/src/App.jsx
+++ GroupProject4/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 
 function App() {
@@ -27,6 +26,7 @@ function App() {
         onChange={(event) => setNewTask(event.target.value)}
       />
       <button onClick={addToList}>Add</button>
+      <p>Number of tasks: {task.length}</p>
       <ul>
         {task.map((item, index) => (
           <li key={index}>
@@ -35,54 +35,6 @@ function App() {
           </li>
         ))}
       </ul>
-=======
-import { useState } from "react";
-
-function App() {
-  const [count, setCount] = useState(0);
-  const [price, setPrice] = useState(0);
-  function submit() {
-    setCount(count + 1);
-    setPrice(price + 50);
-  }
-  function click() {
-    setCount(count - 1);
-    setPrice(price - 50);
-  }
-  function reset() {
-    setCount(count * 0);
-    setPrice(price * 0);
-  }
-
-  function addToCart() {
-    setCount(count + 1);
-    setPrice(price + 50);
-  }
-
-  return (
-    <div>
-      <h1>Shopping Cart</h1>
-      <button onClick={submit}>Increment</button>
-      <button onClick={click}>Decrease</button>
-      <button onClick={reset}>reset</button>
-      <p>Number of Items in ShopingCart</p>
-      <h1> {count} </h1>;<p>Total price of Items in $</p>
-      <h1>{price} </h1>;<h2>list of product</h2>
-      <h2>A shopping cart table</h2>
-      <table>
-        <tr>
-          <th>name</th>
-          <th>price</th>
-          <th>description</th>
-        </tr>
-        <tr>
-          <td>Lollipop</td>
-          <td>$50</td>
-          <td>WALMART</td>
-        </tr>
-      </table>
-      <button onClick={addToCart}>AddToCart</button>
->>>>>>> master
     </div>
   );
 }
